743ef6384c03c2f6e55f05e335e15d80e5522fc5
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2024-09-17 15:27:51 -0600

    Obtener respuesta correcta

--- models/res_partner.py
+++ models/res_partner.py
@@ -23,7 +23,7 @@ class Partner(models.Model):
             if company.pruebas_fel:
                 request_token = "https://felgttestaws.digifact.com.gt/gt.com.fel.api.v3/api/login/get_token"
                 request_tax_info = "https://felgttestaws.digifact.com.gt/gt.com.fel.api.v3/api/SHAREDINFO"
-                
+
             headers = { "Content-Type": "application/json" }
 
             data = {
@@ -40,10 +40,10 @@ class Partner(models.Model):
                 }
                 r = requests.get(request_tax_info+'?NIT={}&DATA1=SHARED_GETINFONITcom&DATA2=NIT|{}&COUNTRY=GT&USERNAME={}'.format(company.vat.replace('-','').zfill(12), vat, company.usuario_fel), headers= headers_nuevos)
                 certificacion_json = r.json()
-                if "RESPONSE" in certificacion_json:
-                    if "NOMBRE" in certificacion_json["RESPONSE"]:
-                        nombre = certificacion_json["RESPONSE"]["NOMBRE"]
-                        nit = certificacion_json["RESPONSE"]["NIT"]
+                if "RESPONSE" in certificacion_json and len(certificacion_json["RESPONSE"]) > 0:
+                    if "NOMBRE" in certificacion_json["RESPONSE"][0]:
+                        nombre = certificacion_json["RESPONSE"][0]["NOMBRE"]
+                        nit = certificacion_json["RESPONSE"][0]["NIT"]
                         return {'nombre': nombre, 'nit': nit}
                     else:
                         return {'nombre': '', 'nit': ''}
